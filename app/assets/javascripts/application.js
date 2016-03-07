be39fb1cef7fab43779727ca16f81f12e66186eb
RJ Singh <rjsingh17@outlook.com>
2016-03-07 09:55:03 -0600

    turbolinks installed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
-
-//= require jquery
 //= require materialize-sprockets
+//= require_tree .
