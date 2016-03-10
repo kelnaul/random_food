16341b53bea85d4f7441a745b4cc6bed6cdbd531
RJ Singh <rjsingh17@outlook.com>
2016-03-10 10:20:18 -0600

    hello

--- config/routes.rb
+++ config/routes.rb
@@ -13,10 +13,9 @@ Rails.application.routes.draw do
   resources :clients
 
 get '/auth/:provider/callback' => 'sessions#create'
-
 get '/signout' => 'sessions#destroy', :as => :signout
-
 get '/signin' => 'sessions#new', :as => :signin
+get '/auth/failure' => 'sessions#failure'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
