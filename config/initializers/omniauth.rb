7560c15304962fdfb6ffe9a77ea9c21d7692f0d6
RJ Singh <rjsingh17@outlook.com>
2016-03-10 10:09:53 -0600

    more fb changes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,11 @@
+
+
 if Rails.env == "production"
   # no production app yet
-  Rails.application.config.middleware.use OmniAuth::Builder do
-      #provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
-      provider :facebook, '1040359132702969', 'c6b3675a3c5f8e72123eff48250cc341'  # Find these values on the Facebook App page
+	Rails.application.config.middleware.use OmniAuth::Builder do
+    	#provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
+		#provider_ignores_state: true,
+    	provider :facebook, '1040359132702969', 'c6b3675a3c5f8e72123eff48250cc341'  # Find these values on the Facebook App page
 	end
 
 else
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   resources :food_types
   resources :users
   resources :clients
+
+  match '/auth/:provider/callback' => 'sessions#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
